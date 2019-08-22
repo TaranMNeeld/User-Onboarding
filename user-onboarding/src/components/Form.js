33796bcc10d3d7787fb9a70bd91dac8632e36a59
Taran <thetaranneeld@gmail.com>
2019-08-22 16:33:52 -0600

    rendered the user data and added some styling

--- user-onboarding/src/components/Form.js
+++ user-onboarding/src/components/Form.js
@@ -1,28 +1,46 @@
-import React, {useState} from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Form, Field, withFormik } from "formik";
 import * as Yup from "yup";
+import "./Form.css";
 
-const UserForm = ({ errors, touched, values }) => {
+const UserForm = ({ errors, touched, values, status }) => {
 
     const [users, setUsers] = useState([]);
 
+    useEffect(() => {
+        if (status) {
+            setUsers([...users, status]);
+        }
+    }, [status]);
+
     return (
         <div>
-            <Form>
+            <Form className="form">
                 {touched.name && errors.name && <p>{errors.name}</p>}
-                <Field type="text" name="name" placeholder="Name" />
+                <Field type="text" name="name" placeholder="Name" className="form-input"/>
                 {touched.email && errors.email && <p>{errors.email}</p>}
-                <Field type="text" name="email" placeholder="Email" />
+                <Field type="text" name="email" placeholder="Email" className="form-input"/>
                 {touched.password && errors.password && <p>{errors.password}</p>}
-                <Field type="password" name="password" placeholder="Password" />
+                <Field type="password" name="password" placeholder="Password" className="form-input"/>
                 <label>
-                    {touched.terms && errors.terms && <p>{errors.terms}</p>}
                     Terms of Service:
-                    <Field type="checkbox" name="terms" placeholder="Terms of Service" checked={values.terms}/>
+                    {touched.terms && errors.terms && <p>{errors.terms}</p>}
+                    <Field type="checkbox" name="terms" placeholder="Terms of Service" checked={values.terms} className="form-input"/>
                 </label>
-                <button>Submit</button>
+                <button type="submit" className="form-input">Submit</button>
             </Form>
+            <div className="user-container">
+            {users.map(user => {
+                return (
+                    <ul key={user.id} className="user-card">
+                        <li>{user.name}</li>
+                        <li>{user.email}</li>
+                        <li>{user.password}</li>
+                    </ul>
+                )
+            })}
+            </div>
         </div >
     );
 };
@@ -41,13 +59,13 @@ const FormickUserForm = withFormik({
         name: Yup.string().max(10, "Name connot be more than 10 characters!").required("Name is required!"),
         email: Yup.string().email("Must be a valid email!").required("Email is required!"),
         password: Yup.string().min(8, "Password must be atleast 8 characters!").required("Password is required!"),
-        terms: Yup.string().required()
+        terms: Yup.boolean().required("Terms of Service required!")
     }),
-    handleSubmit(values) {
+    handleSubmit(values, { setStatus }) {
         axios
             .post("https://reqres.in/api/users/", values)
             .then(res => {
-                console.log(res.data);
+                setStatus(res.data);
             })
             .catch(err => console.log(err.response));
     }
